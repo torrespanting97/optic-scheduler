812109a826f66dcb9749120102e710d4f861dc5e
Ricardo Torres <TorresJesusR2@JohnDeere.com>
2025-11-25 00:57:14 -0600

    fix: remove useCdn import from env module

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
   projectId,
